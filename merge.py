3309ad30a3b10f26b121cbd66215b74d40f73323
Thomas Cetto <thomas.cetto04@gmail.com>
2022-07-28 15:53:38 +0200

    Now the hours under 10 have a 0 before them

--- merge.py
+++ merge.py
@@ -7,7 +7,7 @@ data_folder_path = "C:/Users/thoma/Desktop/DS1/first-challenge/"
 # 2. creates list with files to merge based on name convention
 file_list = [
     data_folder_path + f for f in os.listdir(data_folder_path)
-    if f.startswith('data')
+    if f.startswith('data_20')
 ]
 
 # 3. creates empty list to include the content of each file converted to pandas DF
@@ -18,6 +18,7 @@ ind = 0
 for file in sorted(file_list):
     print(ind)
     ind += 1
+    print(file)
     csv_list.append(
         pd.read_csv(
             file, encoding="cp1252").assign(File_Name=os.path.basename(file)))
@@ -28,25 +29,20 @@ csv_merged = pd.concat(csv_list, ignore_index=True)
 # remove rows with not defined value
 csv_merged = csv_merged[csv_merged.Valore != "n.d."]
 
-
 # sorts the pandas DF by station, pollutant, date and hour
 csv_merged = csv_merged.sort_values(
     by=["Stazione", "Inquinante", "Data", "Ora"])
 
-csv_merged["Data"] = csv_merged["Data"] + "-" + csv_merged["Ora"].astype(str)
-
+csv_merged["Data"] = csv_merged["Data"] + "-" + csv_merged["Ora"].astype(str).str.zfill(2)
 
 # 6. Single DF is saved to the path in CSV format, without index column
-csv_merged.to_csv(save_path + 'APPA_data_merged.csv',
-                  index=False,
-                  columns=[
-                      "Stazione", "Inquinante", "Data", "Valore",
-                      "Unità di misura"
-                  ]
-)
-
+csv_merged.to_csv(
+    save_path + 'APPA_data_merged.csv',
+    index=False,
+    columns=["Stazione", "Inquinante", "Data", "Valore", "Unità di misura"])
 
 import zipfile
-zipfile.ZipFile("APPA_data_merged.zip", mode="w", compression=zipfile.ZIP_DEFLATED).write("APPA_data_merged.csv")
 
-
+zipfile.ZipFile("APPA_data_merged.zip",
+                mode="w",
+                compression=zipfile.ZIP_DEFLATED).write("APPA_data_merged.csv")
